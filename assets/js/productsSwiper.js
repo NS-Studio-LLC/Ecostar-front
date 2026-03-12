0195a28d29ddc6295759b96b1c5e2010b26446e2
aliyev98 <maharram.aliyev98@gmail.com>
2026-03-12 15:05:52 +0400

    homepage finished

---
+++ assets/js/productsSwiper.js
@@ -0,0 +1,20 @@
+new Swiper(".productsSwiper", {
+    slidesPerView: 6,
+    spaceBetween: 22,
+    speed: 500,
+    loop: false,
+    navigation: {
+        nextEl: ".btn-next",
+        prevEl: ".btn-prew",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: { slidesPerView: 1.2, spaceBetween: 14 },
+        520: { slidesPerView: 2.2, spaceBetween: 16 },
+        900: { slidesPerView: 3.2, spaceBetween: 18 },
+        1200: { slidesPerView: 5, spaceBetween: 22 },
+    },
+});
