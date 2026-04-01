8f77aef401da75659d31c3e21dda5b03bd030f4f
aliyev98 <maharram.aliyev98@gmail.com>
2026-04-01 17:49:23 +0400

    product cards swiper, responsive design

--- assets/js/productsSwiper.js
+++ assets/js/productsSwiper.js
@@ -18,3 +18,45 @@ new Swiper(".productsSwiper", {
         1200: { slidesPerView: 5, spaceBetween: 22 },
     },
 });
+
+new Swiper(".recentlyProductsSwiper", {
+    slidesPerView: 6,
+    spaceBetween: 22,
+    speed: 500,
+    loop: false,
+    navigation: {
+        nextEl: ".btn-next",
+        prevEl: ".btn-prew",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: { slidesPerView: 1.2, spaceBetween: 14 },
+        520: { slidesPerView: 2.2, spaceBetween: 16 },
+        900: { slidesPerView: 3.2, spaceBetween: 18 },
+        1200: { slidesPerView: 5, spaceBetween: 22 },
+    },
+});
+
+new Swiper(".similarBlogsSwiper", {
+    slidesPerView: 6,
+    spaceBetween: 22,
+    speed: 500,
+    loop: false,
+    navigation: {
+        nextEl: ".btn-next",
+        prevEl: ".btn-prew",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: { slidesPerView: 1.2, spaceBetween: 14 },
+        520: { slidesPerView: 2.2, spaceBetween: 16 },
+        900: { slidesPerView: 2.2, spaceBetween: 18 },
+        1200: { slidesPerView: 3, spaceBetween: 22 },
+    },
+});
