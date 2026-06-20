1260ad58a1d20a62a4cda04e4b98633dbf1469eb
aliyev98 <maharram.aliyev98@gmail.com>
2026-06-20 14:16:45 +0400

    css fixex and homepage hero changed

--- assets/js/heroSwiper.js
+++ assets/js/heroSwiper.js
@@ -1,11 +1,21 @@
+// var swiper = new Swiper(".heroSwiper", {
+//   slidesPerView: 1,
+//   loop: true,
+//   speed: 500,
+//   autoplay: {
+//     delay: 1500,
+//     disableOnInteraction: false,
+//   },
+// });
+
 var swiper = new Swiper(".heroSwiper", {
-  slidesPerView: 1,
-  loop: true,
-  speed: 500,
-  autoplay: {
-    delay: 1500,
-    disableOnInteraction: false,
-  },
+    slidesPerView: 1,
+    loop: true,
+    speed: 1500,
+    autoplay: {
+        delay: 7000,
+        disableOnInteraction: false,
+    },
 });
 
 var swiper = new Swiper(".aboutHeroSwiper", {
